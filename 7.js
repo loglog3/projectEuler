ece33b3047ec7c6fd063ad3cf5de906e1823d30c
loglog3 <loglog354@gmail.com>
2021-07-13 13:58:59 +0900

    8번 풀고있습니다

--- 7.js
+++ 7.js
@@ -1,18 +1,20 @@
 let count = 0
 let num = 1
-let divider = 1
 
 while(count < 10001) {
     num += 1
     let result
+    // 소수 판별, 본인보다 작은 자연수(1제외)로 나눠지면 안된다
     for(let d = 2; d < num; d++) {
         if(num % d == 0) {
             result = false
             break
         }
     }
+    // 소수가 아닐경우 아무것도 안한다
     if(result == false) {
     } else {
+        //소수일 경우 count 해주고 출력해준다
         count += 1
         console.log(num, count)
     }
